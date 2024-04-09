6af1641fe4ead13171b711103112bc70409fac17
Luigi Carvalho <carvalholuigi25@gmail.com>
2024-04-09 19:33:06 +0100

    made some changes in backend & frontend (pt 2)

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ChatMessage, User } from '@app/models';
 import { SharedModule } from '@app/modules';
 import { AlertsService, AuthService, ChatMessagesService, UsersService } from '@app/services';
+import { environment } from '@environments/environment';
+import * as signalR from '@microsoft/signalr';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -16,13 +20,13 @@ export class ChatComponent implements OnInit {
   targetUserId?: number = 1;
   submitted!: boolean;
   chatMessage: ChatMessage | any;
-  chatMessagesData: ChatMessage[] = [];
+  chatMessagesData$: Observable<ChatMessage[]> = new Observable<ChatMessage[]>();
   usersList: User[] | any = [];
   chatMessageForm!: FormGroup;
   isUserSelected: boolean = false;
   isChatEnabled: boolean = true;
 
-  constructor(private authService: AuthService, private chatMessagesService: ChatMessagesService, private usersService: UsersService, private alertsService: AlertsService) {
+  constructor(private router: Router, private authService: AuthService, private chatMessagesService: ChatMessagesService, private usersService: UsersService, private alertsService: AlertsService) {
     this.authService.user.subscribe((x: any) => {
       this.userId = x.usersInfo.userId;
     });
@@ -32,7 +36,7 @@ export class ChatComponent implements OnInit {
     this.loadChatMessagesForm();
     this.getUsersList();
     this.getChatMessages();
-    this.chatMessagesService.loadSignalRStuff();
+    this.loadSignalRStuff();
   }
 
   get f() { return this.chatMessageForm.controls; }
@@ -55,14 +59,7 @@ export class ChatComponent implements OnInit {
   }
 
   getChatMessages() {
-    this.chatMessagesService.getAll().subscribe({
-      next: (r) => {
-        this.chatMessagesData = r;
-      },
-      error: (err) => {
-        console.error(err.Message);
-      }
-    });
+    this.chatMessagesData$ = this.chatMessagesService.getAll();
   }
 
   talkToThisUser(event: Event, uid: number = 1) {
@@ -76,15 +73,9 @@ export class ChatComponent implements OnInit {
   deleteAll() {
     this.chatMessagesService.deleteAllChatMessages().subscribe({
       next: (v) => {
-        if(this.chatMessagesData.length > 0) {
-          this.alertsService.openAlert(`Deleted all messages!`, 1, "success");
-        } else {
-          this.alertsService.openAlert(`Theres no messages to be deleted!`, 1, "info");
-        }
-
+        this.alertsService.openAlert(`Deleted all messages!`, 1, "success");
         this.chatMessageForm.reset();
         this.getChatMessages();
-        location.reload();
       },
       error: (err) => { 
         console.log(err); 
@@ -123,12 +114,37 @@ export class ChatComponent implements OnInit {
         this.alertsService.openAlert(`Created new message!`, 1, "success");
         this.chatMessageForm.reset();
         this.getChatMessages();
-        location.reload();
       },
       error: (err) => { 
         console.log(err); 
         this.alertsService.openAlert(`Error: ${err}`, 1, "error");
       }
     });
+  }
+
+  loadSignalRStuff() {
+    const hubConnection = new signalR.HubConnectionBuilder()
+        .configureLogging(environment.production ? signalR.LogLevel.Information : signalR.LogLevel.Debug)
+        .withUrl('http://localhost:5001/chathub')
+        .withAutomaticReconnect()
+        .build();
+
+    hubConnection.start().then(function () {  
+        console.log('ChatHub Connected!');
+    }).catch(function (err) {  
+        return console.error(err.toString());  
+    });
+
+    hubConnection.on("SendMessage", () => {
+      this.getChatMessages();
+    });
+
+    hubConnection.onreconnected(() => {
+      this.getChatMessages();
+    });
+  }
+
+  reload() {
+    location.reload();
   }
 }
