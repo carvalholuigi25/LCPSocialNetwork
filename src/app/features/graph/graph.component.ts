1ba774ce5a7c66cbe060c077547bef05b3164323
Luigi Carvalho <carvalholuigi25@gmail.com>
2024-05-06 14:28:07 +0100

    improved the chart (or graph) component

--- src/app/features/graph/graph.component.ts
+++ src/app/features/graph/graph.component.ts
@@ -1,5 +1,7 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService, ThemesService } from '@app/services';
 import Chart, { ChartConfiguration } from 'chart.js/auto';
+import moment from 'moment';
 
 @Component({
   selector: 'app-graph',
@@ -11,12 +13,16 @@ import Chart, { ChartConfiguration } from 'chart.js/auto';
 export class GraphComponent implements OnInit {
   chart: any;
 
-  constructor() { }
+  constructor(private themesService: ThemesService, private authService: AuthService) { }
 
   randomize(min = 1, max = 100) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  pushValIntoDataAry(min = 1, max = 100) {
     var ary = [];
-    for(var k = 0; k < 12; k++) {
-      ary.push(Math.floor(Math.random() * (max - min + 1) + min));
+    for(var k = 0; k < max; k++) {
+      ary.push(this.randomize(min, max));
     }
     return ary;
   }
@@ -25,47 +31,115 @@ export class GraphComponent implements OnInit {
     this.getChart();
   }
 
-  loadConfigGraph(): ChartConfiguration {
-    return {
-      type: "bar",
-      data: {
-         labels: ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], 
-	       datasets: [
-          {
-            label: 'Luigi\'s Posts',
-            data: this.randomize(1, 12),
-            borderColor: 'rgba(255,0,0,1)',
-            backgroundColor: 'rgba(255,0,0,.5)',
-            borderWidth: 2,
-            borderRadius: 5,
-          },
-          {
-            label: 'Mario\'s Posts',
-            data: this.randomize(1, 12),
-            borderColor: 'rgba(0,255,0,1)',
-            backgroundColor: 'rgba(0,255,0,.5)',
-            borderWidth: 2,
-            borderRadius: 5,
-          },
-          {
-            label: 'Guest\'s Posts',
-            data: this.randomize(1, 12),
-            borderColor: 'rgba(0,0,255,1)',
-            backgroundColor: 'rgba(0,0,255,.5)',
-            borderWidth: 2,
-            borderRadius: 5,
-          }
-        ]
+  getMonthsNames() {
+    return ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  }
+
+  getColorText() {
+    return this.themesService.getTheme()?.includes("dark") ? 'white' : 
+    this.themesService.getTheme()?.includes("glass") ? 'white' : 
+    'black';
+  }
+
+  getColorGrid() {
+    return this.themesService.getTheme()?.includes("dark") ? 'rgba(255,255,255,.3)' : 
+    this.themesService.getTheme()?.includes("glass") ? 'rgba(255,255,255,.3)' : 
+    'rgba(0,0,0,.3)';
+  }
+
+  getDataSets(lblsary: string[]) {
+    return [
+      {
+        label: 'Luigi\'s Posts',
+        data: this.pushValIntoDataAry(1, lblsary.length),
+        borderColor: 'rgba(255,0,0,1)',
+        backgroundColor: 'rgba(255,0,0,.5)',
+        borderWidth: 2,
+        borderRadius: 5,
       },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          y: {
-            beginAtZero: true
+      {
+        label: 'Mario\'s Posts',
+        data: this.pushValIntoDataAry(1, lblsary.length),
+        borderColor: 'rgba(0,255,0,1)',
+        backgroundColor: 'rgba(0,255,0,.5)',
+        borderWidth: 2,
+        borderRadius: 5,
+      },
+      {
+        label: 'Guest\'s Posts',
+        data: this.pushValIntoDataAry(1, lblsary.length),
+        borderColor: 'rgba(0,0,255,1)',
+        backgroundColor: 'rgba(0,0,255,.5)',
+        borderWidth: 2,
+        borderRadius: 5,
+      }
+    ];
+  }
+
+  getOptionsGraph(colortxt: string, colorgrid: string = ""): any {
+    var myuinfo = this.authService.getCurUserInfoAuth();
+    var datanow = moment().format("YYYY-MM-DD HH:mm");
+
+    return {
+      responsive: true,
+      maintainAspectRatio: false,
+      color: colortxt,
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: colortxt
+          },
+          grid: {
+            color: colorgrid ?? "rgba(255,255,255,.5)"
+          }
+        },
+        x: {
+          ticks: {
+            color: colortxt
+          },
+          grid: {
+            color: colorgrid ?? "rgba(255,255,255,.5)"
           }
         }
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: 'Posts',
+          color: colortxt,
+          position: 'top',
+          align: 'center',
+          padding: 10
+        },
+        subtitle: {
+          display: true,
+          text: `Fetched data chart by ${myuinfo.username} on ${datanow}`,
+          color: colortxt,
+          position: 'bottom',
+          align: 'center',
+          padding: 10
+        }
       }
+    };
+  }
+
+  getDataGraph(lblsary: string[]) {
+    return {
+      labels: lblsary, 
+      datasets: this.getDataSets(lblsary)
+    };
+  }
+
+  loadConfigGraph(): ChartConfiguration {
+    var lblsary = this.getMonthsNames();
+    var colortxt = this.getColorText();
+    var colorgrid = this.getColorGrid();
+
+    return {
+      type: "bar",
+      data: this.getDataGraph(lblsary),
+      options: this.getOptionsGraph(colortxt, colorgrid)
     };
   }
 
